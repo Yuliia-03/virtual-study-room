b73852a0f941b9b8cea8fe633bee9556c04bd7d1
Yuliia-03 <yuliia.bohak03@gmail.com>
2025-01-31 21:12:31 +0000

    Build the structure of Django project

---
+++ application/api/models/__init__.py
@@ -0,0 +1,7 @@
+from .friend_request import *
+from .rewards import *
+from .session_user import *
+from .study_session import *
+from .todo_list import *
+from .todo_list_user import *
+from .user import *
