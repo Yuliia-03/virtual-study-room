ff2e211cb14891c8049434436e188540efa5faf3
Natalia Ahsan <ahsannatalia@hotmail.com>
2025-02-03 12:05:08 +0000

    add toDoList model

--- application/api/models/todo_list.py
+++ application/api/models/todo_list.py
@@ -1,1 +1,11 @@
 from django.db import models
+class toDoList(models.Model):
+    list_id = models.CharField(max_length=255, primary_key=True)
+    title = models.CharField(max_length=255, blank=False)
+    content = models.CharField(max_length=255)
+    creation_date = models.DateField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+    is_shared = models.BooleanField(default=False)
+
+    def __str__(self):
+        return(f"{self.title}")
