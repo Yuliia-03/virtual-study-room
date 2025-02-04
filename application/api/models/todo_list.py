d4c7425980d7809f5f2ea6ec99595e9927f03f04
Natalia Ahsan <ahsannatalia@hotmail.com>
2025-02-04 13:29:25 +0000

    Add comments and Make few minor changes in the toDoList model

--- application/api/models/todo_list.py
+++ application/api/models/todo_list.py
@@ -1,11 +1,29 @@
 from django.db import models
+
+'''
+toDoList Model:
+    This is a model that manages the toDoList items in the database, each item has various attributes.
+
+Model Breakdown:
+    list_id -> Primary key to uniquely identify each toDoList item in the database.
+    title -> CharField of max 255 characters, to give each toDoList item a title. This is set by the user (and cannot be empty) and stored as an attribute to the toDoList item.
+    content -> CharField but has no limit of characters and can remain empty, this is all set by the user and stored as an attribute.
+    creation_date -> DateField and this automatically sets that field to the current date. This is effectively a read-only field.
+    is_completed -> BooleanField that is set to false by default, this indiacted whether the user has completed the task.
+    is_shared -> BooleanField that is also set to false by default, this field is to mark whether the user wants to share the toDolist item with others.
+
+str Method:
+    This prints out the title and list_id of the toDoList item, in the database, useful for debugging. 
+
+'''
+
 class toDoList(models.Model):
     list_id = models.CharField(max_length=255, primary_key=True)
     title = models.CharField(max_length=255, blank=False)
-    content = models.CharField(max_length=255)
+    content = models.CharField(blank=True)
     creation_date = models.DateField(auto_now_add=True)
     is_completed = models.BooleanField(default=False)
     is_shared = models.BooleanField(default=False)
 
     def __str__(self):
-        return(f"{self.title}")
+        return(f"{self.title} has list_id {self.list_id}")
