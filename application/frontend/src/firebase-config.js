41177a1360c679fe9205e5dae920182f4f42490e
Yonna Khatri <yonnakhatri@gmail.com>
2025-02-05 19:31:17 +0000

    link firebase-config with app, add storage/realtime database

--- application/frontend/src/firebase-config.js
+++ application/frontend/src/firebase-config.js
@@ -1,21 +1,24 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
+
+//web app's firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
+//initialise firebase
 const app = initializeApp(firebaseConfig);
 
-console.log("🔥 Firebase initialized in React:", app);
+//add firebase services
+const storage = getStorage(app);
+const database = getDatabase(app);
 
-export default app;
+export { app, storage, database };
