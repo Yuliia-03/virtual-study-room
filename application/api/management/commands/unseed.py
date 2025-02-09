70e51a7eb34fcad510d7309bb8bd965bfe58b8ea
Yuliia-03 <yuliia.bohak03@gmail.com>
2025-02-09 01:45:22 +0000

    Add seeder for friend request

---
+++ application/api/management/commands/unseed.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+from api.models import *
+
+
+class Command(BaseCommand):
+    """Build automation command to unseed the database."""
+
+    help = 'Unseeds the database by removing all sample data'
+
+    def handle(self, *args, **options):
+        """Unseed the database."""
+        print("Starting database unseeding...")
+
+        # Delete in order to respect foreign key constraints
+        
+
+        print("Deleting friends...")
+        Friends.objects.all().delete()
+
+        print("Deleting users...")
+        User.objects.all().delete()
