c10707c86a4948ef03537844169bc6db00ed9350
Yuliia-03 <yuliia.bohak03@gmail.com>
2025-02-17 23:57:50 +0000

    Move signup logic to separate view file

--- application/api/views/views.py
+++ application/api/views/views.py
@@ -3,38 +3,3 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
-from api.models import User
-from rest_framework.decorators import api_view
-
-@api_view(["POST"])
-def signup(request):
-    try:
-        data = request.data  # DRF automatically parses JSON data
-        firstname = data.get("firstname")
-        lastname = data.get("lastname")
-        username = data.get("username")
-        email = data.get("email")
-        password = data.get("password")
-        password_confirmation = data.get("passwordConfirmation")
-
-        # Validate password match
-        if password != password_confirmation:
-            return Response({"error": "Passwords do not match"}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Check if username already exists
-        if User.objects.filter(username=username).exists():
-            return Response({"error": "Username already taken"}, status=status.HTTP_400_BAD_REQUEST)
-
-        if User.objects.filter(email=email).exists():
-            return Response({"error": "Email already taken"}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Create user
-        user = User.objects.create_user(
-            firstname=firstname, lastname=lastname, username=username, email=email, password=password)
-
-        user.save()
-
-        return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
-
-    except Exception as e:
-        return Response({"error": "Invalid request", "details": str(e)}, status=status.HTTP_400_BAD_REQUEST)
--- application/backend/urls.py
+++ application/backend/urls.py
@@ -21,7 +21,7 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
-from api.views import views
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
