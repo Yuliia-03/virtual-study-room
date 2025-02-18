70152a6fedd9410f9de11dfe62e692c9174d91e8
Aamukta Thogata <aamukta.thogata@gmail.com>
2025-02-18 00:25:17 +0000

    Change Icon, Base Dashboard Code
    Signed-off-by: Aamukta Thogata <aamukta.thogata@gmail.com>

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -3,13 +3,15 @@ import './styles/App.css';
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Welcome from "./pages/Welcome";
+import Dashboard from "./pages/Dashboard";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        // temporary change ok
+        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- application/frontend/src/pages/Dashboard.js
+++ application/frontend/src/pages/Dashboard.js
@@ -1,2 +1,26 @@
 import React from "react"
+import { useNavigate } from "react-router-dom";
+import mangoCat from "../assets/mango_cat.png";
 
+function Dashboard() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="welcome-container">
+            <h1 className="welcome-heading">The Study Spot</h1>
+            <img src={mangoCat} alt="logo" className="welcome-image" />
+            <div className="button-container">
+                <button className="login-button" onClick={() => navigate("/login")}>
+                    LOGIN
+                </button>
+                {/* change this to create account page URL */}
+                <button className="create-account-button" onClick={() => navigate("/signup")}>
+                    CREATE ACCOUNT
+                </button>
+            </div>
+        </div>
+    );
+
+}
+
+export default Dashboard;
