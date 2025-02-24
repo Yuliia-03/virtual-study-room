2df594031adef3cb1a9cf610caa29d0d88ee944c
Agrima Khare <agrima.khare@kcl.ac.uk>
2025-02-24 14:27:35 +0000

    Finish motivational message feature

--- application/api/views/__init__.py
+++ application/api/views/__init__.py
@@ -1,3 +1,4 @@
 from api.views.signup import *
 from api.views.login_page import *
 from api.views.toDoList import ViewToDoList
+from api.views.motivational_message import *
--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './styles/App.css';
+import React from "react";
+import "./styles/App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Welcome from "./pages/Welcome";
 import Dashboard from "./pages/Dashboard";
-import GroupStudyPage from './pages/GroupStudyPage';
+import GroupStudyPage from "./pages/GroupStudyPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MotivationalMessage from "./pages/Motivation";
 //import ToDoList from './pages/ToDoList';
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
+        <Route path="/motivational-message" element={<MotivationalMessage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/group-study" element={<GroupStudyPage />} />
       </Routes>
