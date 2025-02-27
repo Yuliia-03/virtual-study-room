15339f1f62f2b813830d99ca0326913f1785ee74
Agrima Khare <agrima.khare@kcl.ac.uk>
2025-02-27 15:57:49 +0000

    Complete sign up form validation and verification

--- application/api/views/signup.py
+++ application/api/views/signup.py
@@ -11,33 +11,7 @@ from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError, transaction
 
-class NewPasswordMixin:
-    """View mixin for validating new_password and password_confirmation fields."""
-
-    password_regex = re.compile(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$')
-    password_error_message = (
-        "Password must contain an uppercase character, a lowercase character, and a number."
-    )
-
-    def validate_password(self, data):
-        """Validate new_password and password_confirmation fields."""
-        new_password = data.get("new_password")
-        password_confirmation = data.get("password_confirmation")
-        
-        errors = {}
-
-        if not new_password or not password_confirmation:
-            errors["new_password"] = "Both password fields are required."
-
-        elif new_password != password_confirmation:
-            errors["password_confirmation"] = "Confirmation does not match password."
-
-        elif not self.password_regex.match(new_password):
-            errors["new_password"] = self.password_error_message
-
-        return errors
-
-class SignUpView(NewPasswordMixin, APIView):
+class SignUpView( APIView):
 
     def post(self, request):
         try:
@@ -50,37 +24,6 @@ class SignUpView(NewPasswordMixin, APIView):
             password = data.get("password")
             password_confirmation = data.get("passwordConfirmation")
 
-            # Ensuring none of the fields are blank
-            missing_fields = [
-            field for field in ["firstname", "lastname", "username", "email", "password", "passwordConfirmation"]
-            if not data.get(field)
-            ]
-            if missing_fields:
-                return Response({"error": f"Missing required fields: {', '.join(missing_fields)}"}, status=status.HTTP_400_BAD_REQUEST)
-
-            # Validate Email
-            try:
-                validate_email(email)
-            except ValidationError:
-                return Response({"error": "Invalid email format"}, status=status.HTTP_400_BAD_REQUEST)
-
-            # Ensure Passwords match
-            if password != password_confirmation:
-                return Response({"error": "Passwords do not match"}, status=status.HTTP_400_BAD_REQUEST)
-            
-            # Ensure username and email are unique
-            if User.objects.filter(username=username).exists():
-                return Response({"error": "Username already taken"}, status=status.HTTP_400_BAD_REQUEST)
-            if User.objects.filter(email=email).exists():
-                return Response({"error": "Email already taken"}, status=status.HTTP_400_BAD_REQUEST)
-
-            # Validating the password using the Mixin
-            try:
-                self.validate_password(data) 
-            except ValidationError as e:
-                return Response({"error": e.message_dict}, status=status.HTTP_400_BAD_REQUEST)
-
-            # If everything's ok, create the user
             user = User.objects.create_user(
                 firstname=firstname, lastname=lastname, username=username, email=email, description=description, password=password)
             user.full_clean()
--- application/frontend/src/pages/Signup.js
+++ application/frontend/src/pages/Signup.js
@@ -3,133 +3,239 @@ import "../styles/Signup.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-
 function Signup() {
+  //fields that the user will input
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    username: "",
+    description: "",
+    password: "",
+    passwordConfirmation: "",
+    acceptTerms: false,
+  });
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
 
-    //fields that the user will input
-    const [formData, setFormData] = useState({ firstname: "", lastname: "", email: "", username: "", description:"", password: "", passwordConfirmation: "", acceptTerms: false });
-    const navigate = useNavigate();
+  // validating the user input
+  const validate = () => {
+    let newErrors = {};
 
-    //when the fields are edited, update form data
-    const handleChange = (e) => {
-        const { name, type, checked, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: type === "checkbox" ? checked : value
-        });
-    };
+    // checking if no fields are left empty
+    if (!formData.firstname.trim()) {
+      newErrors.firstname = "First Name is required";
+    }
 
-    //when the signup button is clicked - send form data to backend django form
-const handleSignup = async () => {
-        if (!formData.acceptTerms) {
-            alert("You must accept the terms and conditions.");
-            return;
-        }
+    if (!formData.lastname.trim()) {
+      newErrors.lastname = "Last Name is required";
+    }
 
-        try {
-            const response = await axios.post("http://127.0.0.1:8000/api/signup/", formData, {
-                headers: { "Content-Type": "application/json" }
-            });
-            
-            alert(response.data.message); // Show success message
-            navigate('/dashboard');
-        } catch (error) {
-            if (error.response) {
-                alert(error.response.data.error); 
-            } else {
-                //console.error("Signup error:", error);
-                alert("An error occurred. Please try again.");
-            }
-        }
-    };
+    // using regex to check the username, email, and password are in the correct format
+    const usernameRegex = /^@\w{3,}$/;
+    if (!formData.username.trim()) {
+      newErrors.username = "Username is required";
+    } else if (!usernameRegex.test(formData.username)) {
+      newErrors.username =
+        "Username must consist of @ followed by at least three alphanumericals";
+    }
 
-    return (
-        <div className="login-container">
-            <h1 className="heading1">The Study Spot</h1>
-            <form className="signup-form">
-                <h1 className="heading2">Signup</h1>
-                <label htmlFor="firstname" className="label-text">First name:</label>
-                <input
-                    id= "firstname"
-                    type="text"
-                    name="firstname"
-                    className="input-field"
-                    value={formData.firstname}
-                    onChange={handleChange}
-                />
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!emailRegex.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+    }
 
-                <label htmlFor="lastname" className="label-text">Last name:</label>
-                <input
-                    id="lastname"
-                    type="text"
-                    name="lastname"
-                    className="input-field"
-                    value={formData.lastname}
-                    onChange={handleChange}
-                />
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$/;
+    if (!formData.password.trim()) {
+      newErrors.password = "Password is required";
+    } else if (!passwordRegex.test(formData.password)) {
+      newErrors.password =
+        "Password must contain an uppercase character, a lowercase character, and a number.";
+    } else if (
+      formData.password.trim() !== formData.passwordConfirmation.trim()
+    ) {
+      newErrors.password = "Password confirmation needs to match password";
+    }
 
-                <label htmlFor="username" className="label-text">Username:</label>
-                <input
-                    id="username"
-                    type="text"
-                    name="username"
-                    className="input-field"
-                    value={formData.username}
-                    onChange={handleChange}
-                />
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
 
-                <label htmlFor="email" className="label-text">Email:</label>
-                <input
-                    id="email"
-                    type="text"
-                    name="email"
-                    className="input-field"
-                    value={formData.email}
-                    onChange={handleChange}
-                />
+  //when the fields are edited, update form data
+  const handleChange = (e) => {
+    const { name, type, checked, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
 
-                <label htmlFor="details" className="label-text">Your motto in life :):</label>
-                <input
-                    id="details"
-                    type="text"
-                    name="description"
-                    className="input-field"
-                    value={formData.description}
-                    onChange={handleChange}
-                />
+  //when the signup button is clicked - send form data to backend django form
+  const handleSignup = async () => {
+    if (!formData.acceptTerms) {
+      alert("You must accept the terms and conditions.");
+      return;
+    }
 
-                <label htmlFor="password" className="label-text">Password:</label>
-                <input
-                    id="password"
-                    type="password"
-                    name="password"
-                    className="input-field"
-                    value={formData.password}
-                    onChange={handleChange}
-                />
+    try {
+      if (validate()) {
+        const response = await axios.post(
+          "http://127.0.0.1:8000/api/signup/",
+          formData,
+          {
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+        alert(response.data.message);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.error);
+      } else {
+        //console.error("Signup error:", error);
+        alert("An error occurred. Please try again.");
+      }
+    }
+  };
 
-                <label htmlFor="passwordConfirmation" className="label-text">Confirm password:</label>
-                <input
-                    id="passwordConfirmation"
-                    type="password"
-                    name="passwordConfirmation"
-                    className="input-field"
-                    value={formData.passwordConfirmation}
-                    onChange={handleChange}
-                />
-
-                <div className="checkbox-container">
-                    <input type="checkbox" name="acceptTerms" id="acceptTerms"
-                        checked={formData.acceptTerms} onChange={handleChange} />
-                    <label htmlFor="acceptTerms" className="checkbox-label">
-                        I accept the <a href="#">terms and conditions</a>
-                    </label>
-                </div>
-
-                <button type="button" className="submit-button" onClick={handleSignup}>SIGNUP</button>
-            </form>
+  return (
+    <div className="login-container">
+      <h1 className="heading1">The Study Spot</h1>
+      <form className="signup-form">
+        <div className="field">
+          <h1 className="heading2">Signup</h1>
+          <label htmlFor="firstname" className="label-text">
+            First name:
+          </label>
+          <input
+            id="firstname"
+            type="text"
+            name="firstname"
+            className="input-field"
+            value={formData.firstname}
+            onChange={handleChange}
+          />
+          {errors.firstname && (
+            <p className="error-message">{errors.firstname}</p>
+          )}
         </div>
-    );
+
+        <div className="field">
+          <label htmlFor="lastname" className="label-text">
+            Last name:
+          </label>
+          <input
+            id="lastname"
+            type="text"
+            name="lastname"
+            className="input-field"
+            value={formData.lastname}
+            onChange={handleChange}
+          />
+          {errors.lastname && (
+            <p className="error-message">{errors.lastname}</p>
+          )}
+        </div>
+
+        <div className="field">
+          <label htmlFor="username" className="label-text">
+            Username:
+          </label>
+          <input
+            id="username"
+            type="text"
+            name="username"
+            className="input-field"
+            value={formData.username}
+            onChange={handleChange}
+          />
+          {errors.username && (
+            <p className="error-message">{errors.username}</p>
+          )}
+        </div>
+
+        <div className="field">
+          <label htmlFor="email" className="label-text">
+            Email:
+          </label>
+          <input
+            id="email"
+            type="text"
+            name="email"
+            className="input-field"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p className="error-message">{errors.email}</p>}
+        </div>
+
+        <div className="field">
+          <label htmlFor="details" className="label-text">
+            Your motto in life :):
+          </label>
+          <input
+            id="details"
+            type="text"
+            name="description"
+            className="input-field"
+            value={formData.description}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="field">
+          <label htmlFor="password" className="label-text">
+            Password:
+          </label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className="input-field"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          {errors.password && (
+            <p className="error-message">{errors.password}</p>
+          )}
+        </div>
+
+        <div className="field">
+          <label htmlFor="passwordConfirmation" className="label-text">
+            Confirm password:
+          </label>
+          <input
+            id="passwordConfirmation"
+            type="password"
+            name="passwordConfirmation"
+            className="input-field"
+            value={formData.passwordConfirmation}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="checkbox-container">
+          <input
+            type="checkbox"
+            name="acceptTerms"
+            id="acceptTerms"
+            checked={formData.acceptTerms}
+            onChange={handleChange}
+          />
+          <label htmlFor="acceptTerms" className="checkbox-label">
+            I accept the <a href="#">terms and conditions</a>
+          </label>
+        </div>
+
+        <button type="button" className="submit-button" onClick={handleSignup}>
+          SIGNUP
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
