8738d75b2267ebf2881afe60bf49a52be68b797a
Yonna Khatri <yonnakhatri@gmail.com>
2025-03-02 15:19:42 +0000

    add full coverage tests

--- application/frontend/src/tests/App.test.js
+++ application/frontend/src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
+jest.mock('../firebase-config.js') ;
 test('renders welcome heading', () => {
     render(<App />);
     const headingElement = screen.getByText(/The Study Spot/i); // Or any text present in Welcome.js
