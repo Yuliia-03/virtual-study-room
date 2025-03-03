ea4bf562791a3fe6e5955685112d456ede8006ce
Isha Selvakumaran <isha.selvakumaran@gmail.com>
2025-03-03 12:20:00 +0000

    Add analytics box to dashbaord and write tests for analytics api

--- application/backend/urls.py
+++ application/backend/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('api/signup/', views.signup, name='signup'),
     path("api/analytics/", get_analytics, name="analytics"),  # Default for logged-in user
-    path("api/analytics/<str:username>/", get_analytics, name="analytics_by_user"),  # Fetch by username
     path('api/login/', views.login, name='login'),
     path('api/todolists/<str:is_shared>/', views.ViewToDoList.as_view(), name='toDoList'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
