79231d255a42cf23515d5112a678d150e18250ea
Yuliia-03 <yuliia.bohak03@gmail.com>
2025-03-10 02:43:38 +0000

    Create Friends component

---
+++ application/api/views/friends.py
@@ -0,0 +1,32 @@
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from api.models import Friends, Status, Permission
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.views import View
+from rest_framework.permissions import IsAuthenticated
+
+
+class FriendsView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        friendships = Friends.get_friends_with_status(
+            user_1=user, status=Status.ACCEPTED)
+        friends = [friend.user2 if friend.user1 ==
+                   user else friend.user1 for friend in friendships]
+
+        response_data = []
+        for user in friends:
+            response_data.append({
+                "id": user.id,
+                "name": user.firstname,
+                "surname": user.lastname,
+                "username": user.username
+            })
+
+        return Response(response_data, status=status.HTTP_200_OK)
--- application/frontend/src/pages/Dashboard.js
+++ application/frontend/src/pages/Dashboard.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import "../styles/Dashboard.css";
 import ToDoList from '../components/ToDoListComponents/ToDoList';
 import Analytics from './Analytics';
+import Friends from '../components/Friends';
 
 function Dashboard() {
     return (
@@ -19,8 +20,7 @@ function Dashboard() {
                 </div>
                 <div class = "dashboard-main-panel">
                     <div class="dashboard-panel">Profile</div>
-                    <div class="dashboard-panel">Friends List</div>
-                    <div class="dashboard-panel">Add Friends</div>
+                    <div class="dashboard-panel"><Friends/></div>
                 </div>
                 <div class = "dashboard-right-panel">
                     <div class="dashboard-panel">Generate Group Study Room</div>
