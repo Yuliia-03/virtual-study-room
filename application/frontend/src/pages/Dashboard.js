42799eecaa054db87629a2a5e22526cb1e189e5b
Yuliia-03 <yuliia.bohak03@gmail.com>
2025-03-11 01:48:24 +0000

    Add friends components on dashboard

--- application/frontend/src/pages/Dashboard.js
+++ application/frontend/src/pages/Dashboard.js
@@ -4,8 +4,7 @@ import ToDoList from '../components/ToDoListComponents/ToDoList';
 
 import StudyRoomComponent from '../components/StudyRoomComponent';
 import Analytics from './Analytics';
-import Friends from '../components/friends/Friends';
-import PendingFriends from '../components/friends/PendingFriends';
+import  FriendsTab  from '../components/friends/FriendsTab';
 
 
 function Dashboard() {
@@ -19,13 +18,11 @@ function Dashboard() {
             <div className = "dashboard-content">
                 <div className = "dashboard-left-panel" data-testid="left-panel">
                     <Analytics />
-                    {/*<div className="dashboard-panel">Calendar</div>*/}
-                    <div className="dashboard-panel"><PendingFriends/></div>
+                    <div className="dashboard-panel">Calendar</div>
                 </div>
                 <div class = "dashboard-main-panel">
                     <div class="dashboard-panel">Profile</div>
-                    <div class="dashboard-panel"><Friends/></div>
-                    {/*<div class="dashboard-panel">Add Friends</div>*/}
+                    <div class="dashboard-panel"><FriendsTab /></div>
                 </div>
                 <div className = "dashboard-right-panel" data-testid="right-panel">
                     <StudyRoomComponent />
