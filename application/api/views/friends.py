5ae6c186615870169c47da33f7e988ff271ac3fa
Yuliia-03 <yuliia.bohak03@gmail.com>
2025-03-12 16:55:59 +0000

    Update friends view to handle user search

--- application/api/views/friends.py
+++ application/api/views/friends.py
@@ -1,10 +1,7 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from api.models import Friends, Status, Permission
-from rest_framework.decorators import api_view
+from api.models import Friends, Status, User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.views import View
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -28,6 +25,24 @@ class FriendsView(APIView):
         elif url_name == "friends_requested":
             print("Friends requested")
             friends = Friends.get_invitations_sent(user)
+
+        elif url_name == "find_friend":
+            print("Searching for a new friends...")
+            search_query = request.GET.get('q', '')
+
+            users = User.find_user(search_query)
+            print(users)
+
+            response_data = []
+            for user in users:
+                response_data.append({
+                    "id": user.id,
+                    "name": user.firstname,
+                    "surname": user.lastname,
+                    "username": user.username
+                })
+            return Response(response_data, status=status.HTTP_200_OK)
+
         return self.get_friends(request, friends)
 
     def get_friends(self, request, data):
