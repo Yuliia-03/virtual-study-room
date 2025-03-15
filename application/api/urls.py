a315270d2483b3969f0c7e4890c2c066f44eb234
Natalia Ahsan <ahsannatalia@hotmail.com>
2025-03-15 01:25:06 +0000

    Add spotify API backend stuff

--- application/api/urls.py
+++ application/api/urls.py
@@ -3,14 +3,17 @@ from .views.login_page import login
 from rest_framework.routers import DefaultRouter
 #from .views.calendar import list_events
 from .views.calendar import EventViewSet
-from .views.spotify_view import AuthURL
+from .views.spotify_view import AuthURL, spotify_callback, IsAuthenticated
 
 router = DefaultRouter()
 # router.register(r'events', list_events, basename='event')
 router.register(r'events', EventViewSet, basename='event')
 
+
 urlpatterns = [
     #path('views/login-page', login),
     path('', include(router.urls)),
-    path('get-auth-url', AuthURL.as_view())
+    path('get-auth-url', AuthURL.as_view()),
+    path('redirect', spotify_callback), 
+    path('is-authenticated', IsAuthenticated.as_view())
 ]
--- application/api/utils.py
+++ application/api/utils.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 from calendar import HTMLCalendar
 from .models.events import Event
+from .models.spotify_token import SpotifyToken
+from django.utils import timezone
 
 class Calendar(HTMLCalendar):
 	def __init__(self, year=None, month=None):
@@ -38,3 +40,25 @@ class Calendar(HTMLCalendar):
 		for week in self.monthdays2calendar(self.year, self.month):
 			cal += f'{self.formatweek(week, events)}\n'
 		return cal
+	
+
+class Spotify_API():
+	def get_user_tokens(self, session_id):
+		user_tokens = SpotifyToken.objects.filter(user=session_id)
+		if user_tokens.exists():
+			return user_tokens
+		else:
+			return None
+		
+	def update_or_create_user_tokens(self, session_id, access_token, token_type, expires_in, refresh_token):
+		tokens = self.get_user_tokens(session_id)
+		expires_in = timezone.now() + timedelta(seconds=expires_in)
+		if tokens:
+			tokens.access_token = access_token
+			tokens.refresh_token = refresh_token
+			tokens.expires_in = expires_in
+			tokens.token_type = token_type
+			tokens.save(update_fields = ['access_token', 'refresh_token', 'expires_in', 'token_type'])
+		else:
+			tokens = SpotifyToken(user=session_id, access_token=access_token, refresh_token=refresh_token, token_type=token_type, expires_in=expires_in)
+			tokens.save()
